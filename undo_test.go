05a1e43791cc18e8852b5b4292a25fd029ca27ee
reus <reusee@gmail.com>
2014-12-03 13:41:09 +0800

    add undo test

--- undo_test.go
+++ undo_test.go
@@ -1,6 +1,10 @@
 package ebuf
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/reusee/rope"
+)
 
 func TestUndo(t *testing.T) {
 	b := New(nil)
@@ -29,5 +33,31 @@ func TestRedo(t *testing.T) {
 	b.Redo()
 	if b.Current != 2 {
 		t.Fatal("current")
+	}
+}
+
+func TestUndoRedoWithEdit(t *testing.T) {
+	b := New(nil)
+	b.Insert(0, []byte("foo"))
+
+	b.Undo()
+	b.Insert(0, []byte("bar")) // redo is cleared
+	if len(b.Events) != 3 {
+		t.Fatal("events length")
+	}
+	if b.Current != 2 {
+		t.Fatal("current")
+	}
+	if string(b.Events[b.Current].(*rope.Rope).Bytes()) != "bar" {
+		t.Fatal("string of index 2")
+	}
+
+	b.Undo()
+	if len(b.Events) != 3 {
+		t.Fatal("events length")
+	}
+	b.Delete(0, 0)
+	if len(b.Events) != 3 {
+		t.Fatal("events length")
 	}
 }
